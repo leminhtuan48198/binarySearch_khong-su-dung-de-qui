5283fc0591d60eeea695dab525f76a1e28e8c665
Tuan le <leminhtuan48198@gmail.com>
2022-08-22 09:57:05 +0700

    first commit

---
+++ src/BinarySearch.java
@@ -0,0 +1,29 @@
+public class BinarySearch {
+    static int[] list = {2, 4, 7, 10, 11, 45, 50, 59, 60, 66, 69, 70, 79};
+
+    public static int binarySearch(int[] list, int key) {
+        int low = 0;
+        int hight = list.length - 1;
+
+        while (low <= hight) {
+            int mid = (low + hight) / 2;
+            if (key < list[mid]) {
+                hight = mid - 1;
+            } else if (key == list[mid]) {
+                return mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(binarySearch(list, 2));  /* 0 */
+        System.out.println(binarySearch(list, 11)); /* 4 */
+        System.out.println(binarySearch(list, 79)); /*12 */
+        System.out.println(binarySearch(list, 1));  /*-1 */
+        System.out.println(binarySearch(list, 5));  /*-1 */
+        System.out.println(binarySearch(list, 80)); /*-1 */
+    }
+}
